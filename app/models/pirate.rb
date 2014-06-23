d523cec4cdf706409413b5534f23409a4036ceab
kyfast <kylie.stradley@gmail.com>
2014-06-22 20:59:05 -0400

    Full text multi-column search

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,5 +1,7 @@
 class Pirate < ActiveRecord::Base
   def self.search(query)
-      where("name like :q", q: "%#{query}%") 
+    # where("name like :q", q: "%#{query}%") 
+    # where("name ilike :q or description ilike :q or ship ilike :q", q: "%#{query}%")
+    where("name @@ :q or description @@ :q or ship @@ :q", q: query)
     end
 end
