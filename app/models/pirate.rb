943d4e7ca3e22cd55dfb1a70aa140138f41867c7
kyfast <kylie.stradley@gmail.com>
2014-06-23 23:07:54 -0400

    Proper fulltext & ranked search

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -4,8 +4,12 @@ class Pirate < ActiveRecord::Base
       # where("name like :q", q: "%#{query}%") 
       # where("name ilike :q or description ilike :q or ship ilike :q", q: "%#{query}%")
       # where("name @@ :q or description @@ :q or ship @@ :q", q: query)
-      where("name @@ :q or description @@ :q or ship @@ :q", q: query).order("#{rank} desc")
-      end
+      rank = <<-RANK
+        ts_rank(to_tsvector(name), plainto_tsquery(#{sanitize(query)})) +
+        ts_rank(to_tsvector(ship), plainto_tsquery(#{sanitize(query)})) +
+        ts_rank(to_tsvector(description), plainto_tsquery(#{sanitize(query)}))
+      RANK
+      where("name @@ :q or description @@ :q", q: query).order("#{rank} desc")
     else
       all
     end
