577e342ed430fa698b8d6bb07bba0d8f62f1852f
kyfast <kylie.stradley@gmail.com>
2014-06-23 23:58:16 -0400

    Add more pirates to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,10 @@ pirates = Pirate.create([
   { name: 'Dread Pirate Roberts', ship: 'As You Wish', description: 'Devilishly handsome, highly resistant to iocaine powder.'},
   { name: 'William Kidd', ship: 'Adventure Galley', description: 'Somewhat unreliable pirate hunter. Abandoned his ship. Executed in 1701'},
   { name: 'Blackbeard', ship: 'Queen Anne\'s revenge', description: 'Blockaded the port of Charleston. May have deliberately abandoned ship to kill of some crew to keep more treasure'},
-  { name: 'Hector Barbossa', ship: 'Black Pearl', description: 'Prone to mutiny. Tendency to be undead. Likes jokes. Has his own monkey'}
-  ])
+  { name: 'Hector Barbossa', ship: 'Black Pearl', description: 'Prone to mutiny. Tendency to be undead. Likes jokes. Has his own monkey'},
+  { name: 'Jack Sparrow', ship: 'Black Pearl', description: 'Deceitful, not particularly famous. Has a full beard and long hair'},
+  { name: 'Davy Jones', ship: 'The Flying Dutchman', description: 'Has a beard made of tentacles. Fair gambler, unfair captain. Tends to mope around.'},
+  { name: 'Jean Lafitte', ship: 'Goelette la Dilidente', description: 'French by birth, supported United States in the battle of New Orleans'},
+  { name: 'Black Bart', ship: 'The Royal Fortune', description: 'A welshman with a penchant for rules and order, he was promoted to captain of his ship by vote. '},
+  { name: 'Redbeard', ship: 'Revenge', description: 'Has a habit of setting adrift the crews of ships he attacks. Was the subject of a Daniel Defoe story.'},
+  { name: 'Sam Bellamy', ship: 'Whydah', description: 'He joined the navy early in life, but was disatisfied. Pirated a ship with Blackbeard before they both set out on their own.'}])
