a180b78450aacf5cc7a0e771b1245a6998877726
kyfast <kylie.stradley@gmail.com>
2014-06-23 23:06:43 -0400

    If no search params, display all pirates

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,7 +1,12 @@
 class Pirate < ActiveRecord::Base
   def self.search(query)
-    # where("name like :q", q: "%#{query}%") 
-    # where("name ilike :q or description ilike :q or ship ilike :q", q: "%#{query}%")
-    where("name @@ :q or description @@ :q or ship @@ :q", q: query)
+    if query.present?
+      # where("name like :q", q: "%#{query}%") 
+      # where("name ilike :q or description ilike :q or ship ilike :q", q: "%#{query}%")
+      # where("name @@ :q or description @@ :q or ship @@ :q", q: query)
+      where("name @@ :q or description @@ :q or ship @@ :q", q: query).order("#{rank} desc")
+      end
+    else
+      all
     end
 end
